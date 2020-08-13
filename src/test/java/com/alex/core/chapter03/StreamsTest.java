3d3955ac1763e7cec862f9809e14c0d87ef68bce
Alejandro Calzada <alejandro.calzada.algravez@gmail.com>
2020-08-12 19:11:14 -0500

    Adjust streams unit tests

--- src/test/java/com/alex/core/chapter03/StreamsTest.java
+++ src/test/java/com/alex/core/chapter03/StreamsTest.java
@@ -2,11 +2,14 @@ package com.alex.core.chapter03;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import com.alex.core.Album;
 import com.alex.core.Track;
 
 class StreamsTest {
@@ -54,6 +57,23 @@ class StreamsTest {
 
 		Streams streams = new Streams();
 		Assertions.assertEquals(12, streams.reduces(numbers));
+
+	}
+
+	@Test
+	void testFindLongTracks() {
+		List<Track> tracks = new ArrayList<>();
+		tracks.add(new Track("Bakai", 70));
+		tracks.add(new Track("Violets for Your Furs", 60));
+		tracks.add(new Track("Time Was", 50));
+
+		List<Album> albums = new ArrayList<>();
+		albums.add(new Album(tracks));
+
+		Streams streams = new Streams();
+		Set<String> trackNames = new HashSet<>();
+		trackNames.add("Bakai");
+		Assertions.assertEquals(trackNames, streams.findLongTracks(albums));
 
 	}
 
